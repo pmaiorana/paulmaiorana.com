6e863f3bc0932d65dd6c1e2cb884df78cf49e7d6
Paul Maiorana <paul@paulmaiorana.com>
2014-11-24 13:17:38 -0500

    Adding twentyeleven and my child theme thereof

---
+++ themes/paulmaiorana/content-single.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying content in the single.php template
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'twentyeleven' ) . '</span>', 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-meta">
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( __( ', ', 'twentyeleven' ) );
+
+			/* translators: used between list items, there is a space after the comma */
+			$tag_list = get_the_tag_list( '', __( ', ', 'twentyeleven' ) );
+			if ( '' != $tag_list ) {
+				$utility_text = __( '%2$s', 'twentyeleven' );
+			} elseif ( '' != $categories_list ) {
+				$utility_text = __( '', 'twentyeleven' );
+			} else {
+				$utility_text = __( '', 'twentyeleven' );
+			}
+
+			printf(
+				$utility_text,
+				$categories_list,
+				$tag_list,
+				esc_url( get_permalink() ),
+				the_title_attribute( 'echo=0' ),
+				get_the_author(),
+				esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) )
+			);
+		?>
+	</footer><!-- .entry-meta -->
+	<?php edit_post_link( __( 'Edit', 'twentyeleven' ), '<span class="edit-link">', '</span>' ); ?>
+</article><!-- #post-<?php the_ID(); ?> -->
